19f50db9d16319638a9049707ebd47a08980bbd7
tangerinem <redvelvet0625@gmail.com>
2026-04-18 00:49:07 +0900

    feat: create SwaggerConfiguration

---
+++ src/main/java/com/likelion14/swagger/SwaggerConfiguration.java
@@ -0,0 +1,31 @@
+package com.likelion14.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("LIKELION KNU Animal League API")
+                .description("LIKELION KNU Animal League API 문서예요.")
+                .version("v1.0.0")
+                .license(new License().name("MIT"));
+
+        Server localServer = new Server()
+                .url("http://localhost:8080")
+                .description("Local Server");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(localServer));
+    }
+}
